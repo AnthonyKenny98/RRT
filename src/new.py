4c0c4a0d3bb5d41941a7afde5d1b1ec1e99d0577
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-01-05 03:19:03 -0800

    Now in good place to try go to 3D

--- src/new.py
+++ src/new.py
@@ -3,40 +3,49 @@
 # @Author: AnthonyKenny98
 # @Date:   2020-01-05 20:46:51
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2020-01-05 21:52:28
+# @Last Modified time: 2020-01-05 22:18:23
 
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as np
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d', xlim=100, ylim=100, zlim=100)
-ax.invert_zaxis()
+
+def rrt_config():
+    """Get configuration params from params.h file."""
+    params = {
+        'XDIM': 100,
+        'YDIM': 100,
+        'ZDIM': 100,
+        'RESOLUTION': 5
+    }
+
+    # Read params.h
+    # with open('params.h', 'r') as file:
+    #     for line in file:
+    #         params[line.split(' ')[1]] = int(line.split(' ')[2].strip('\n'))
+    return params
 
 
-def plot_rectangle(origin, ax):
-    w = 10
-    l = 10
-    h = 10
+def get_ogm(params):
+    """."""
+    ogm = np.zeros((params['XDIM'], params['YDIM'], params['ZDIM']))
 
+    ogm[3:6, 3:6, 3:6] = 1
+
+    return ogm
+
+
+def plot_rectangle(origin, s, ax):
+    """."""
     add = np.array([[0, 0, 0],
-                    [0, l, 0],
-                   [w, l, 0],
-                   [w, 0, 0],
-                   [0, 0, h],
-                   [0, l, h],
-                   [w, l, h],
-                   [w, 0, h]])
+                    [0, s, 0],
+                   [s, s, 0],
+                   [s, 0, 0],
+                   [0, 0, s],
+                   [0, s, s],
+                   [s, s, s],
+                   [s, 0, s]])
 
-    p = np.array([[10, 10, 10],
-                  [10, 20, 10],
-                  [20, 20, 10],
-                  [20, 10, 10],
-                  [10, 10, 20],
-                  [10, 20, 20],
-                  [20, 20, 20],
-                  [20, 10, 20]
-                  ])
     p = origin + add
 
     def draw_face(p1):
@@ -46,7 +55,8 @@ def plot_rectangle(origin, ax):
 
         verts = [list(zip(x, y, z))]
 
-        collection = Poly3DCollection(verts, linewidths=1, edgecolors='black', zsort='min')
+        collection = Poly3DCollection(
+            verts, linewidths=1, edgecolors='black', zsort='min')
         face_color = "red"
         collection.set_facecolor(face_color)
         ax.add_collection3d(collection)
@@ -59,8 +69,38 @@ def plot_rectangle(origin, ax):
     draw_face(np.array([p[1], p[2], p[6], p[5]]))
 
 
-origin = np.array([10, 10, 10])
-plot_rectangle(origin, ax)
-origin = np.array([30, 30, 30])
-plot_rectangle(origin, ax)
-plt.show()
+def main():
+    """."""
+    params = rrt_config()
+
+    # Create Figure
+    fig = plt.figure("Configuration Space")
+
+    # Init plot in 3D with correct axis limits
+    ax = fig.add_subplot(
+        111, projection='3d',
+        xlim=params['XDIM'], ylim=params['YDIM'], zlim=params['ZDIM'])
+
+    # Invert every axis to give correct cartesian look
+    ax.invert_xaxis()
+    ax.invert_yaxis()
+    ax.invert_zaxis()
+
+    # Get Occupancy Grid Map from RRT
+    ogm = get_ogm(params)
+
+    # Draw Occupied Grids
+    for i in range(int(params['XDIM'] / params['RESOLUTION'])):
+        for j in range(int(params['YDIM'] / params['RESOLUTION'])):
+            for k in range(int(params['ZDIM'] / params['RESOLUTION'])):
+                if ogm[i][j][k]:
+                    origin = [i * params['RESOLUTION'],
+                              j * params['RESOLUTION'],
+                              k * params['RESOLUTION']]
+                    plot_rectangle(origin, params['RESOLUTION'], ax)
+
+    # Show Plot
+    plt.show()
+
+if __name__ == "__main__":
+    main()
