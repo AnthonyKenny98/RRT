3cbd1a4cda31290d80722546ce911fcf829ac03e
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-02-18 11:49:42 -0800

    Clean

--- src/params.h
+++ src/params.h
@@ -2,7 +2,6 @@
 #define YDIM 16
 #define ZDIM 16
 #define EPSILON 4
-#define NUM_NODES 100
+#define NUM_NODES 1000
 #define RESOLUTION 1
-#define NUMBUCKETS 200 // Max out at DIM^2 * 3/2 (Max distance from center)
-
+#define NUMBUCKETS 10 // Max out at DIM^2 * 3/2 (Max distance from center)
