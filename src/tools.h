676c9b272ae07c9d83b377c025147ee63cf9e07c
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-03-20 10:46:35 +1100

    remove extra .c files, now only reliant on .h files

--- src/tools.h
+++ src/tools.h
@@ -40,14 +40,26 @@ typedef struct edge {
 // Function Declarations
 
 // Returns a Random float. Requires srand(time(NULL)) to be called in main
-float randomfloat(int max);
+float randomfloat(int max) {
+    return ((float)rand() / (float)RAND_MAX) * max;
+}
 
 
 // Euclidean Distance between two points
-float distance_squared(point_t p1, point_t p2);
+float distance_squared(point_t p1, point_t p2) {
+    return ((p1.x-p2.x)*(p1.x-p2.x)) + 
+           ((p1.y-p2.y)*(p1.y-p2.y)) +
+           ((p1.z-p2.z)*(p1.z-p2.z));
+}
 
 
 // Returns a random node in the state space
-point_t getRandomNode();
+point_t getRandomNode() {
+    return (point_t) {.x = randomfloat(XDIM - 1),
+                      .y = randomfloat(YDIM - 1),
+                      .z = randomfloat(ZDIM - 1)};
+}
 
-point_t newPoint();
+point_t newPoint() {
+    return (point_t) {.x = 0., .y=0., .z=0.};
+}
