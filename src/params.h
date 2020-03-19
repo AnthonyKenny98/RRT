b804d7ed9bcc5375b55d2633d453244da8330a37
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-03-20 09:34:06 +1100

    More testing

--- src/params.h
+++ src/params.h
@@ -1,8 +1,8 @@
 #define XDIM 32
 #define YDIM 32
 #define ZDIM 32
-#define EPSILON 4
-#define NUM_NODES 1000
+#define EPSILON 8
+#define NUM_NODES 500
 #define RESOLUTION 1
-#define NUMBUCKETS 1
+#define NUMBUCKETS 2
 #define GOAL_BIAS 25
