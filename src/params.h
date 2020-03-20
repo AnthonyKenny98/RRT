e882c02446914111c50b223ae41723eacffc2b31
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-03-20 13:46:05 +1100

    New template

--- src/params.h
+++ src/params.h
@@ -1,8 +1,8 @@
 #define XDIM 16
 #define YDIM 16
 #define ZDIM 16
-#define EPSILON 2
-#define NUM_NODES 100
+#define EPSILON 1
+#define NUM_NODES 250
 #define RESOLUTION 1
 #define NUMBUCKETS 2
-#define GOAL_BIAS 25
+#define GOAL_BIAS 0
